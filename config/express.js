6e55e747070271096ebec595d7d784951e9a2b89
Lars Boegild Thomsen <lth@netcompartner.com>
2016-01-25 11:31:29 +0800

    Adding passport

---
+++ config/express.js
@@ -0,0 +1,28 @@
+var config = require('./config'),
+    express = require('express'),
+    passport = require('passport'),
+    bodyParser = require('body-parser');
+
+module.exports = function() {
+    var app = express();
+
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+
+    app.use(bodyParser.json());
+
+    app.set('views', './app/views');
+    app.set('view engine', 'ejs');
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    require('../app/routes/index.server.routes.js')(app);
+    require('../app/routes/users.server.routes.js')(app);
+
+    app.use(express.static('./public'));
+
+    return app;
+};
+
