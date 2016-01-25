263439c0fa33ea0249fb2cc9be4d1f0bef461285
Lars Boegild Thomsen <lth@netcompartner.com>
2016-01-25 12:37:58 +0800

    Hmmmmm - mostly working that is

--- config/express.js
+++ config/express.js
@@ -8,23 +8,23 @@ var config = require('./config'),
 module.exports = function() {
     var app = express();
 
-    app.use(session({
-        saveUninitialized: true, 
-        resave: true, 
-        secret: 'OutSuperSecretCookingSecret'
-    }));
-
-    app.use(flash());
-
     app.use(bodyParser.urlencoded({
         extended: true
     }));
 
     app.use(bodyParser.json());
 
+    app.use(session({
+        saveUninitialized: true, 
+        resave: true, 
+        secret: 'OutSuperSecretCookingSecret'
+    }));
+
+
     app.set('views', './app/views');
     app.set('view engine', 'ejs');
 
+    app.use(flash());
     app.use(passport.initialize());
     app.use(passport.session());
 
